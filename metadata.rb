fb3c7c1ca031c7c611a09b1d956cc8fd28baaad4
Fabio Napoleoni <f.napoleoni@gmail.com>
2013-02-11 19:14:38 +0100

    Update package cache before doing anything

--- metadata.rb
+++ metadata.rb
@@ -6,3 +6,5 @@ description 'Installs/Configures system  base packages and some other low level 
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.0'
 
+# this dependency is used in Debian/Ubuntu to ensure that the package cache is up to date
+depends 'apt', '~> 1.7.0'
--- recipes/default.rb
+++ recipes/default.rb
@@ -5,6 +5,16 @@
 # Copyright (C) 2013 Fabio Napoleoni
 #
 
+# Update package cache
+if node[:system_base][:packages].any?
+  case node['platform_family']
+    when 'debian'
+      include_recipe 'apt'
+    else
+      # do nothing
+  end
+end
+
 # install package specified in attributes
 # see http://wiki.opscode.com/display/chef/Resources#Resources-Package
 node[:system_base][:packages].each do |p|
