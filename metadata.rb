ac5d35bc0e66196b8517dae588ce4a92a1ed41f6
Fabio Napoleoni <f.napoleoni@gmail.com>
2013-02-12 02:32:54 +0100

    Version bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'f.napoleoni@gmail.com'
 license 'All rights reserved'
 description 'Installs/Configures system  base packages and some other low level stuff'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.0'
+version '0.1.1'
 
 # this dependency is used in Debian/Ubuntu to ensure that the package cache is up to date
 depends 'apt', '~> 1.7.0'
