8c27c83fce8895cd486cf0cd49249bef2a87c18b
Fabio Napoleoni <f.napoleoni@gmail.com>
2013-06-24 15:42:39 +0200

    Added changelog and version bump to 0.1.4

---
+++ CHANGELOG.md
@@ -0,0 +1,14 @@
+## 0.1.4
+
+* Added missing license
+* Added `mysql-config` recipe to configure some mysql aspects for shell usage
+* Added right global configuration files for zsh ( > 5.0.0 )
+* Global git config now includes `sharedRepository = group`
+
+## 0.1.3
+
+* Added `secrets` recipe to merge encrypted databags into node attributes
+
+## 0.1.0
+
+Initial version
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'f.napoleoni@gmail.com'
 license          'Apache 2.0'
 description 'Installs/Configures system  base packages and some other low level stuff'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.3'
+version '0.1.4'
 
 # this dependency is used in Debian/Ubuntu to ensure that the package cache is up to date
 depends 'apt', '~> 1.7.0'
