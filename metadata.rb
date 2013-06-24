15edaa223b750a3849415d352acf716a5985388f
Fabio Napoleoni <f.napoleoni@gmail.com>
2013-06-24 15:41:54 +0200

    Apache license for all stuff

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,14 @@
 name 'system_base'
 maintainer 'Fabio Napoleoni'
 maintainer_email 'f.napoleoni@gmail.com'
-license 'All rights reserved'
+license          'Apache 2.0'
 description 'Installs/Configures system  base packages and some other low level stuff'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.3'
 
 # this dependency is used in Debian/Ubuntu to ensure that the package cache is up to date
 depends 'apt', '~> 1.7.0'
+
+%w(ubuntu debian).each do |os|
+  supports os
+end
